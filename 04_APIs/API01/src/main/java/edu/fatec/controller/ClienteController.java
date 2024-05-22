ceff370bcbb027217e98cb620d203f807e04b642
Otávio Augusto <otavio208augustoribeiro@gmail.com>
2024-05-22 14:00:05 -0300

    API feita antes de API_Pedidos. Essa foi com o intuito de trinar os metodos necessarios com Clientes antes de fazer a de Pedidos completa

---
+++ 04_APIs/API01/src/main/java/edu/fatec/controller/ClienteController.java
@@ -0,0 +1,109 @@
+package edu.fatec.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.fatec.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	private List<Cliente> listaClientes = new ArrayList<Cliente>();
+	
+	@PostMapping(path = "/clientes") //cadastrar clientes 
+	public ResponseEntity<?> cadastrarCliente(@RequestBody Cliente cliente) {
+		for (Cliente c : listaClientes) {
+			if (c.getEmail().equals(cliente.getEmail())) {
+				return ResponseEntity.status(HttpStatus.CONFLICT).body("Cliente já existe!");
+			}
+		}
+		
+		Integer codigo = (int) (Math.random() * 1000);
+		
+		cliente.setCodigo(codigo);
+		
+		//salva lista na memoria
+		this.listaClientes.add(cliente);
+		
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(cliente);
+	}
+	
+//	@GetMapping(path = "/clientes") //consulta todos os clientes
+//	public List<Cliente> consultarCliente() {
+//
+//		return this.listaClientes;
+//	}
+	
+	@GetMapping(path = "/clientes/{id}") //consulta determinado cliente por id
+	public ResponseEntity<?> consultarClientePorId(@PathVariable(name = "id") Integer codigo) {
+		Cliente clienteProcurado = null;
+		
+		for (Cliente c : this.listaClientes) {
+			if (codigo.equals(c.getCodigo())) {
+				clienteProcurado = c;
+			}
+		}
+		
+		if (clienteProcurado == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		} else {
+			return ResponseEntity.ok(clienteProcurado);
+		}
+	}
+	
+	@GetMapping(path = "/clientes") //consulta clientes por nome, "chave valor"
+	public List<Cliente> consutarClientePorNome(@RequestParam(name = "nome", required = false) String nome) {
+		List<Cliente> filtro = new ArrayList<Cliente>();
+		
+		if (nome != null && !nome.isEmpty()) {
+			for (Cliente c : this.listaClientes) {
+				if (c.getNome().contains(nome)) {
+					filtro.add(c);
+				}
+			}
+			
+			return filtro;
+		}
+		
+		return this.listaClientes;
+	}
+	
+	@DeleteMapping(path = "/clientes/{id}") //deletar cliente
+	public ResponseEntity<?> deletarClientePorId(@PathVariable(name = "id") Integer codigo) {
+		
+		for (Cliente c : this.listaClientes) {
+			if (c.getCodigo().equals(codigo)) {
+				this.listaClientes.remove(c);
+				return ResponseEntity.status(HttpStatus.OK).body("removeu");
+			}
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("nao achou");
+	}
+	
+	@PutMapping(path = "/clientes/{id}") //faz alteração de determinado cliente que foi buscado pelo id
+	public ResponseEntity<?> alterarClientePorId(@PathVariable(name = "id") Integer codigo, @RequestBody Cliente cliente) {
+		
+		for (Cliente c : this.listaClientes) {
+			if (c.getCodigo().equals(codigo)) {
+				c.setEmail(cliente.getEmail());
+				c.setNome(cliente.getNome());
+				
+				return ResponseEntity.status(HttpStatus.OK).body(c);
+			}
+		}
+		
+		return null;
+	}
+	
+}
---
+++ 04_APIs/API01/src/main/java/edu/fatec/model/Cliente.java
@@ -0,0 +1,26 @@
+package edu.fatec.model;
+
+public class Cliente {
+	private Integer codigo;
+	private String nome, email;
+	
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
